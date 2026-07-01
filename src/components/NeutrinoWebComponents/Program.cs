4f28973234d1001ae63d83f7b28279d37e2c4bed
Dmitry Kolchev <dmitrykolchev@msn.com>
2026-07-01 11:02:57 +0300

    add serilog

--- src/components/NeutrinoWebComponents/Program.cs
+++ src/components/NeutrinoWebComponents/Program.cs
@@ -1,3 +1,6 @@
+using System.Globalization;
+using Microsoft.AspNetCore.HttpOverrides;
+using Serilog;
 namespace LitSample;
 
 public class Program
@@ -6,10 +9,38 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Information()
+            .MinimumLevel.Override("Microsoft", Serilog.Events.LogEventLevel.Warning)
+            .MinimumLevel.Override("Microsoft.Hosting.Lifetime", Serilog.Events.LogEventLevel.Information)
+            .ReadFrom.Configuration(builder.Configuration)
+            .Enrich.FromLogContext()
+            .Enrich.WithClientIp() 
+            .WriteTo.Console(formatProvider: CultureInfo.CurrentCulture)
+            .CreateLogger();
+
+        // Use Serilog as the logging provider
+        builder.Host.UseSerilog();
+
+#if !DEBUG
+        builder.Host.UseWindowsService();
+#endif
+
         // Add services to the container.
         builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
+        builder.Services.AddHttpContextAccessor();
 
         var app = builder.Build();
+
+        // Настройка чтения заголовков X-Forwarded-For и X-Forwarded-Proto
+        app.UseForwardedHeaders(new ForwardedHeadersOptions
+        {
+            ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto,
+            KnownIPNetworks = { },
+            KnownProxies = { }
+        });
+        // Configure HTTP Request logging via Serilog
+        app.UseSerilogRequestLogging();
 
         // Configure the HTTP request pipeline.
         if (!app.Environment.IsDevelopment())
@@ -19,7 +50,7 @@ public class Program
             app.UseHsts();
         }
 
-        app.UseHttpsRedirection();
+        //app.UseHttpsRedirection();
         app.UseStaticFiles();
         //app.UseDirectoryBrowser();
 
